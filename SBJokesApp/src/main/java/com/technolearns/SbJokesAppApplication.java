2f7dba96fa79c296b93f1c165ab8a307416ae12a
arjunxxx <arjunrajeev1@gmail.com>
2021-05-24 22:50:19 +0530

    initial commit

---
+++ SBJokesApp/src/main/java/com/technolearns/SbJokesAppApplication.java
@@ -0,0 +1,13 @@
+package com.technolearns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbJokesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbJokesAppApplication.class, args);
+	}
+
+}
---
+++ SBJokesApp/src/test/java/com/technolearns/SbJokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technolearns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbJokesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
